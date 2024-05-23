685a022f97e7ef3543921713f508c1e0867f8f4c
bishopscode <bishopslow01@gmail.com>
2024-05-23 12:38:08 +0100

    added SearchBar.jsx

--- MernStack-Tour-Management-main/MernStack-Tour-Management-main/tour-management/frontend/src/pages/Home.jsx
+++ MernStack-Tour-Management-main/MernStack-Tour-Management-main/tour-management/frontend/src/pages/Home.jsx
@@ -38,12 +38,12 @@ const Home = () => {
           </div>
         </Col>
         <Col lg='2'>
-          <div className="hero__img-box">
+          <div className="hero__img-box mt-4">
             <video src={heroVideo} alt="" controls/>
           </div>
         </Col>
         <Col lg='2'>
-          <div className="hero__img-box">
+          <div className="hero__img-box mt-5">
             <img src={heroImg02} alt="" />
           </div>
         </Col>
