d62349835eaecd1e857cb8597371b38a16227f59
bishopscode <bishopslow01@gmail.com>
2024-05-28 00:46:05 +0100

    worked on the newsletter, footer,home

--- MernStack-Tour-Management-main/MernStack-Tour-Management-main/tour-management/frontend/src/components/Image-gallery/MasonryImagesGallery.jsx
+++ MernStack-Tour-Management-main/MernStack-Tour-Management-main/tour-management/frontend/src/components/Image-gallery/MasonryImagesGallery.jsx
@@ -8,6 +8,7 @@ const MasonryImagesGallery = () => {
             <Masonry gutter="1rem">
                 {galleryImages.map((item, index) => (
                     <img
+                    className="masonry__img"
                         src={item}
                         key={index}
                         alt=""
